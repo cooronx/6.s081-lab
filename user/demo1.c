9a55fb858a9dbc273bbe6b933885c4ee9859fc6f
cooronx <2197083441@qq.com>
2023-10-15 12:32:23 +0800

    dmo1

--- user/demo1.c
+++ user/demo1.c
@@ -1,7 +1,22 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
+int check(int mid){
+    if(mid == 1)return 0;
+    if(mid == 2)return 1;
+    for(int i = 2;i*i <= mid;++i){
+        if(mid % i == 0){
+            return 0;
+        }
+    } 
+    return 1;
+}
+
+
 int main(){
+
+    printf("%d\n",check(5));
+
     demo();
     exit(0);
 }
